56095c32a542e273f33d6be170c3b2999703eb72
rjb255 <rjb255@cantab.ac.uk>
2026-01-12 00:20:47 +0000

    feat: hidden projects

--- src/App.vue
+++ src/App.vue
@@ -80,7 +80,7 @@ onUnmounted(() => {
 const links = [
   { text: "Home", name: "home" },
   { text: "Albums", name: "albums" },
-  { text: "Projects", name: "projects" },
+  // { text: "Projects", name: "projects" },
 ];
 
 const roots = links.map((_, index) => {
